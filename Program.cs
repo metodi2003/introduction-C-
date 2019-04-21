fc2c30ae208a4dac59266bf5cf69c9467c9166f8
metodi2003 <49056546+metodi2003@users.noreply.github.com>
2019-04-21 19:40:37 +0300

    introduction C#
    introduction C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
